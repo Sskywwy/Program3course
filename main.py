496cabc5a758a451cf4ac787f3237c8d907edfd4
sskywwy <sskywwy@gmail.com>
2026-02-04 22:31:02 +0200

    .

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-hello world
-=======
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -14,4 +11,3 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
->>>>>>> c24a19bb12638181ca4672b4af532c5503b2efff
